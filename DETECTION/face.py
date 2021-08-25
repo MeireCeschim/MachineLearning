f3cf5444e9805367a377c776a9fcacbc6715d380
Melissa Palhano <46391712+melpalhano@users.noreply.github.com>
2021-08-25 01:33:18 -0300

    face.py

--- DETECTION/face.py
+++ DETECTION/face.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 
-
 faceClassifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 img = cv2.imread("detection2Learn.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
